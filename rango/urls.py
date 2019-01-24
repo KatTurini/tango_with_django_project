07f129a9fcd2a43d89d0502b7ef3db9e19817de0
Katarina Turinicova <2148256t@student.gla.ac.uk>
2019-01-24 23:30:14 +0000

    chapter 3

--- rango/urls.py
+++ rango/urls.py
@@ -20,9 +20,10 @@ from django.contrib import admin
 from django.conf.urls import include
 from rango import views
 
+from django.conf.urls import url
+from rango import views
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^about', views.about, name='about'),
 ]
