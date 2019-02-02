9f905440cc9aad296aa5bcb0a1ebe96250dc0a9c
Katarina Turinicova <2148256t@student.gla.ac.uk>
2019-02-02 21:28:47 +0000

    chapter 8.1 and 8.2

--- rango/urls.py
+++ rango/urls.py
@@ -23,6 +23,7 @@ from rango import views
 from django.conf.urls import url
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
