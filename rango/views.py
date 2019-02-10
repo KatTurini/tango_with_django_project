098206639b56c7e7eb01d65b1c5acdb068bf0765
Katarina Turinicova <2148256t@student.gla.ac.uk>
2019-02-10 19:43:59 +0000

    ch 10 - 10.5 included

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 @login_required
@@ -77,15 +78,38 @@ def register(request):
                    'registered': registered})
 
 
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
+
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {'categories': category_list, 'pages': page_list}
-    return render(request, 'rango/index.html', context_dict)
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    visitor_cookie_handler(request, response)
+
+    return response
+    # return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_dict = {'boldmessage': "Media file of a cat:"}
     return render(request, 'rango/about.html', context=context_dict)
 
